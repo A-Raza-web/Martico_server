b8e3846bc3df2d8c91962f99665bd7bd9c662301
Ahmad Raza <ar7386039@email.com>
2026-04-13 17:07:35 +0500

    order working...

--- routes/payment.js
+++ routes/payment.js
@@ -34,8 +34,8 @@ router.post("/checkout", async (req, res) => {
             payment_method_types: ["card"],
             line_items: line_items,
             mode: "payment",
-            success_url: "https://martico-server.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: "https://martico-server.vercel.app/cancel",
+            success_url: "https://martico-client.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url: "https://martico-client.vercel.app/cancel",
         });
 
         res.json({ url: session.url });
