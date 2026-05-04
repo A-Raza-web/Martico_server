825732f2efa3ead3288d16a9c60111ddf3247bb6
Ahmad Raza <ar7386039@gmail.com>
2026-05-04 10:05:34 -0400

    Update order.js

--- routes/order.js
+++ routes/order.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/order");
 const Product = require("../models/product");
-const protect = require("../middleware/authMiddleware");
-const {adminOnly } = require("../middleware/authMiddleware");
+const { protect, admin: adminOnly } = require("../middleware/authMiddleware");
 
 function generateOrderNumber() {
   const date = new Date();
